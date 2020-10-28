baea9bc8132c2505ef3fe8cd6853c83b6c98f9b2
Conall OCallaghan <cocallaghan@nvidia.com>
2020-10-28 11:50:43 -0400

    further cleanup of retry backoff

--- internal/internal.go
+++ internal/internal.go
@@ -13,16 +13,20 @@ func RetryBackoff(retry int, minBackoff, maxBackoff time.Duration) time.Duration
 	if minBackoff == 0 {
 		return 0
 	}
+
 	// capping this to avoid d becoming a negative duration
-	if retry > 10 {
-		retry = 10
+	if retry > 63 {
+		retry = 63
+	}
+	d := minBackoff << uint(retry)
+	if d < minBackoff {
+		return maxBackoff
 	}
 
-	d := minBackoff << uint(retry)
 	d = minBackoff + time.Duration(rand.Int63n(int64(d)))
 
 	if d > maxBackoff || d < minBackoff {
-		d = maxBackoff
+		return maxBackoff
 	}
 
 	return d
